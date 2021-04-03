e70f786704687ed1928e1a9df24be330b049d5c6
Whimsicott <eruhoon@gmail.com>
2021-04-03 22:10:40 +0900

    add ToastList

---
+++ src/app/view/common/ToastList.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<main>
+  <!-- <div class="toast" *ngFor="let toast of getToasts()"> -->
+  <div class="toast">toast</div>
+  <!-- {{ toast.getText() }} -->
+</main>
+
+<style lang="scss">
+</style>

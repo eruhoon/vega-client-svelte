1a1a4de6a617685f0592156351338026d5a41e71
엘풍 <eruhoon@gmail.com>
2021-08-13 22:13:55 +0900

    update ChatUserEntry

--- src/app/view/chat/user/ChatUserEntry.svelte
+++ src/app/view/chat/user/ChatUserEntry.svelte
@@ -4,7 +4,6 @@
   export let prop: ChatUserEntryProp = {
     nickname: '',
     icon: '',
-    level: 0,
     flag: {
       mobile: false,
       computer: false,
@@ -24,12 +23,49 @@
     <div class="nickname">
       <span>{prop.nickname} /</span>
     </div>
-    <div class="level">
-      <span>Lv.{prop.level}</span>
-    </div>
   </div>
   <div class="icon">
     <i class="material-icons {mobileClass}">phone_iphone</i>
     <i class="material-icons {computerClass}">computer</i>
   </div>
 </div>
+
+<style lang="scss">
+  .user-icon {
+    width: 30px;
+    height: 30px;
+    overflow: hidden;
+    float: left;
+    border-radius: 30px;
+
+    img {
+      width: 30px;
+      height: 30px;
+    }
+  }
+
+  .info {
+    width: calc(100%- 70px);
+    height: 30px;
+    float: left;
+    padding-left: 5px;
+
+    div {
+      width: auto;
+      float: left;
+      padding-top: 8px;
+    }
+  }
+
+  .icon {
+    width: 20%;
+    height: 30px;
+    float: right;
+
+    i {
+      font-size: 16px;
+      padding-top: 7px;
+      float: right;
+    }
+  }
+</style>

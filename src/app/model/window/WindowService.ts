4b0adc07145ef0c8bf20ecfefe7a8f53b5ffde80
엘풍 <eruhoon@gmail.com>
2021-08-26 00:34:45 +0900

    make WindowService class

--- src/app/model/window/WindowService.ts
+++ src/app/model/window/WindowService.ts
@@ -1,21 +1,14 @@
 import { Writable, writable } from 'svelte/store';
 
-export const WindowService: WindowServiceInit = {
-  userListShow: writable(false),
-  emojiAttachViewShow: writable(false),
-  sideBarShow: writable(false),
-  settingMenuShow: writable(false),
-  siteSettingModalShow: writable(false),
-  streamSettingModalShow: writable(false),
-  profileSettingModalShow: writable(false),
-};
+class WindowServiceInit {
+  userListShow = writable(false);
+  emojiAttachViewShow = writable(false);
+  sideBarShow = writable(false);
+  settingMenuShow = writable(false);
+  siteSettingModalShow = writable(false);
+  streamSettingModalShow = writable(false);
+  profileSettingModalShow = writable(false);
+  modal = writable(null);
+}
 
-type WindowServiceInit = {
-  readonly userListShow: Writable<boolean>;
-  readonly emojiAttachViewShow: Writable<boolean>;
-  readonly sideBarShow: Writable<boolean>;
-  readonly settingMenuShow: Writable<boolean>;
-  readonly siteSettingModalShow: Writable<boolean>;
-  readonly streamSettingModalShow: Writable<boolean>;
-  readonly profileSettingModalShow: Writable<boolean>;
-};
+export const WindowService = new WindowServiceInit();

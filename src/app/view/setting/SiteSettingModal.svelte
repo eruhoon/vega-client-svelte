45201f0307abc87df23aec923d4fd159c4f3b0cd
엘풍 <eruhoon@gmail.com>
2021-08-29 19:11:33 +0900

    extract SiteSettingPreferenceView

--- src/app/view/setting/SiteSettingModal.svelte
+++ src/app/view/setting/SiteSettingModal.svelte
@@ -2,6 +2,7 @@
   import { get } from 'svelte/store';
   import { OptionService } from '../../model/option/OptionService';
   import SettingModal from './SettingModal.svelte';
+  import SiteSettingPreferenceView from './site/SiteSettingPreferenceView.svelte';
 
   let enableTimestamp = get(OptionService.timestamp);
 
@@ -14,19 +15,10 @@
 
 <SettingModal title="사이트 설정" icon="fas fa-tools">
   <div slot="body" class="site-set-box">
-    <div class="set-title">
-      <p>채팅 시각 표기</p>
-    </div>
-    <div class="set-switch">
-      <label
-        for="enableTimestamp"
-        class="switch"
-        class:active={enableTimestamp}
-        on:click={toggleTimestamp}
-      >
-        <span class="slider" />
-      </label>
-    </div>
+    <SiteSettingPreferenceView
+      enable={enableTimestamp}
+      onClick={toggleTimestamp}
+    />
   </div>
 </SettingModal>
 
@@ -37,88 +29,5 @@
     height: 40px;
     padding: 5px 10px;
     border-bottom: 1px solid #1f2226;
-
-    /* 잠시 꺼둠
-          &:last-child{
-            border-bottom: 0px;
-          }
-          &.max-w {
-            height: 150px;
-          }
-          */
-    .set-title {
-      width: 80%;
-      height: 42px;
-      float: left;
-
-      p {
-        font-size: 18px;
-        padding: 12px;
-        color: #ffffff;
-      }
-    }
-
-    .set-switch {
-      width: calc(20% - 30px);
-      height: 42px;
-      float: right;
-      margin-right: 12px;
-      // padding-left: 30px;
-
-      .switch {
-        position: relative;
-        display: inline-block;
-        width: 60px;
-        height: 33px;
-        margin-top: 3px;
-      }
-
-      .switch input {
-        opacity: 0;
-        width: 0;
-        height: 0;
-      }
-
-      .slider {
-        position: absolute;
-        cursor: pointer;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: #1f2226;
-
-        -webkit-transition: 0.4s;
-        transition: 0.4s;
-        border-radius: 33px;
-      }
-
-      .slider:before {
-        position: absolute;
-        content: '';
-        height: 28px;
-        width: 28px;
-        left: 5px;
-        bottom: 3px;
-        background-color: #ffffff;
-        -webkit-transition: 0.4s;
-        transition: 0.4s;
-        border-radius: 50%;
-      }
-
-      .active .slider {
-        background-color: #ff4081;
-      }
-
-      input:focus + .slider {
-        box-shadow: 0 0 1px #ff4081;
-      }
-
-      .active .slider:before {
-        -webkit-transform: translateX(24px);
-        -ms-transform: translateX(24px);
-        transform: translateX(24px);
-      }
-    }
   }
 </style>
---
+++ src/app/view/setting/site/SiteSettingPreferenceView.svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+  export let enable: boolean = false;
+  export let onClick = () => {};
+</script>
+
+<div on:click={onClick}>
+  <div class="set-title">
+    <p>채팅 시각 표기</p>
+  </div>
+  <div class="set-switch">
+    <label for="enable" class="switch" class:active={enable}>
+      <span class="slider" />
+    </label>
+  </div>
+</div>
+
+<style lang="scss">
+  .set-title {
+    width: 80%;
+    height: 42px;
+    float: left;
+
+    p {
+      font-size: 18px;
+      padding: 12px;
+      color: #ffffff;
+    }
+  }
+
+  .set-switch {
+    width: calc(20% - 30px);
+    height: 42px;
+    float: right;
+    margin-right: 12px;
+    // padding-left: 30px;
+
+    .switch {
+      position: relative;
+      display: inline-block;
+      width: 60px;
+      height: 33px;
+      margin-top: 3px;
+    }
+
+    .slider {
+      position: absolute;
+      cursor: pointer;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: #1f2226;
+
+      -webkit-transition: 0.4s;
+      transition: 0.4s;
+      border-radius: 33px;
+    }
+
+    .slider:before {
+      position: absolute;
+      content: '';
+      height: 28px;
+      width: 28px;
+      left: 5px;
+      bottom: 3px;
+      background-color: #ffffff;
+      -webkit-transition: 0.4s;
+      transition: 0.4s;
+      border-radius: 50%;
+    }
+
+    .active .slider {
+      background-color: #ff4081;
+    }
+
+    .active .slider:before {
+      -webkit-transform: translateX(24px);
+      -ms-transform: translateX(24px);
+      transform: translateX(24px);
+    }
+  }
+</style>

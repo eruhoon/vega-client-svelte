20068fa67e25771cc454b6676ffc87df63cfe07f
w2story(이건희) <w2story@naver.com>
2021-08-30 22:25:53 +0900

    Imagepack-Style-Update

--- src/app/view/chat/pack/ImagePack.svelte
+++ src/app/view/chat/pack/ImagePack.svelte
@@ -15,5 +15,9 @@
 <style lang="scss">
   .image {
     width: 100%;
+    background: #1f2226;
+    border: 1px solid rgb(10, 12, 14);
+    border-radius: 2px;
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
   }
 </style>

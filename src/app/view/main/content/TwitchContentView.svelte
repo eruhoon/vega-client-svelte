557f1ada8bfdc6fe56062cc6c3cfe8ee197858bf
엘풍 <eruhoon@gmail.com>
2021-09-01 12:24:04 +0900

    update TwitchContentView

--- src/app/view/main/content/TwitchContentView.svelte
+++ src/app/view/main/content/TwitchContentView.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
   import IframeContentView from './IframeContentView.svelte';
+
   export let src: string | null = null;
-  $: link = src
-    ? `https://player.twitch.tv/?channel=${src}&parent=${location.hostname}`
-    : null;
+  $: link = src ? getLink(src) : null;
+
+  const host = 'https://player.twitch.tv';
+
+  const getLink = (src: string | null) => {
+    return `${host}?channel=${src}&parent=${location.hostname}`;
+  };
 </script>
 
 <IframeContentView src={link} allowfullscreen={true} />

9c3b5d5bb8e53b3c315758aeafae505353184f04
엘풍 <eruhoon@gmail.com>
2021-09-06 19:11:02 +0900

    remove redundant style

--- src/app/view/modal/image/UploadImageChatModal.svelte
+++ src/app/view/modal/image/UploadImageChatModal.svelte
@@ -89,46 +89,5 @@
       border-bottom-right-radius: 5px;
       margin-top: 4px;
     }
-
-    .modal-close {
-      width: 40px;
-      height: 40px;
-      position: absolute;
-      top: calc(0% - 15px);
-      right: calc(0% - 15px);
-
-      background-color: #fafafa;
-      border: 1px solid #f5f5f5;
-      border-radius: 50%;
-      box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
-        0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
-
-      text-align: center;
-
-      i {
-        font-size: 24px;
-        line-height: 42px;
-        padding-left: 2px;
-        color: #424242;
-      }
-
-      &:hover {
-        background-color: #ff4081;
-        border: 1px solid #ff4081;
-
-        i {
-          color: #ffffff;
-        }
-      }
-
-      &:active {
-        background-color: #e91e63;
-        border-color: #e91e63;
-
-        i {
-          color: #ffffff;
-        }
-      }
-    }
   }
 </style>

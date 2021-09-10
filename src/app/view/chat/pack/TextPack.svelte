0f4995cb99f649d58e353093e48733b710934852
엘풍 <eruhoon@gmail.com>
2021-09-11 04:20:03 +0900

    apply Emoji to TextPack

--- src/app/view/chat/pack/TextPack.svelte
+++ src/app/view/chat/pack/TextPack.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+  import { EmojiUtils } from '../../../model/chat/emoji/EmojiUtils';
+
   export let body = '';
+
+  $: isEmoji = EmojiUtils.isEmoji(body);
 </script>
 
 <div>
-  <p>{body}</p>
+  <p class:emoji={isEmoji}>{body}</p>
 </div>
 
 <style lang="scss">
@@ -14,6 +18,12 @@
       width: 100%;
       font-size: 16px;
       line-height: 22px;
+
+      &.emoji {
+        font-size: 50px;
+        line-height: 50px;
+        text-align: center;
+      }
     }
   }
 </style>

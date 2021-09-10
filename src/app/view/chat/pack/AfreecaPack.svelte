784d9b44b5ba4dff98aab5bc190bd4ebbb778776
엘풍 <eruhoon@gmail.com>
2021-09-10 16:36:56 +0900

    extract ExternalStream from AfreecaPack

--- src/app/view/chat/pack/AfreecaPack.svelte
+++ src/app/view/chat/pack/AfreecaPack.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { WindowService } from '../../../model/window/WindowService';
+  import ExternalStreamPack from './ExternalStreamPack.svelte';
 
   export let body: string;
   let json: Param;
@@ -31,67 +32,11 @@
   };
 </script>
 
-<div
-  class="stream-pack"
-  on:click={onClick}
-  on:contextmenu|preventDefault={onContextMenu}
->
-  <img class="icon" src={icon} alt={name} />
-  <div class="logo" />
-  <div class="name">
-    {name} <span>({id})</span>
-  </div>
-</div>
-
-<style lang="scss">
-  .stream-pack {
-    position: relative;
-    width: auto;
-    max-width: 400px;
-    height: 50px;
-    background: #0545b1;
-    align-items: center;
-    justify-content: center;
-    border-radius: 25px;
-    cursor: pointer;
-  }
-
-  .icon {
-    position: absolute;
-    top: 5px;
-    left: 5px;
-    width: 40px;
-    height: 40px;
-    border-radius: 25px;
-  }
-  .logo {
-    position: absolute;
-    background: url('/../assets/image/stream/afreeca.png') center no-repeat;
-    background-size: 100%;
-    top: 15px;
-    left: 50px;
-    width: 20px;
-    height: 20px;
-  }
-  .name {
-    position: absolute;
-    top: 15px;
-    left: 75px;
-    width: calc(100% - 100px);
-    height: 20px;
-    font-size: 14px;
-    color: white;
-    line-height: 20px;
-
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    word-wrap: normal;
-    word-break: break-all;
-    text-overflow: ellipsis;
-    overflow: hidden;
-
-    span {
-      font-size: 12px;
-    }
-  }
-</style>
+<ExternalStreamPack
+  {name}
+  {id}
+  {icon}
+  {onClick}
+  {onContextMenu}
+  backgroundColor="#0545b1"
+/>

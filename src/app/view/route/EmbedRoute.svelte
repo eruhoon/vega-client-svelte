3a0dc0a66a2f399ae9b8c043550896fa7af62069
엘풍 <eruhoon@gmail.com>
2021-09-14 21:57:52 +0900

    update EmbedRoute

--- src/app/view/route/EmbedRoute.svelte
+++ src/app/view/route/EmbedRoute.svelte
@@ -1,11 +1,21 @@
 <script>
+  import LocalStreamContentView from '../main/content/LocalStreamContentView.svelte';
+  import TotoroStreamContentView from '../main/content/TotoroStreamContentView.svelte';
+
   const urlParams = new URLSearchParams(window.location.search);
   const hasId = urlParams.has('id');
   const id = urlParams.get('id');
+  const platform = urlParams.get('p');
 </script>
 
 {#if hasId}
-  <p>{id}</p>
+  {#if platform === 'local'}
+    <LocalStreamContentView src={id} />
+  {:else if platform === 'totoro'}
+    <TotoroStreamContentView src={id} />
+  {:else}
+    <p>error</p>
+  {/if}
 {:else}
   <p>error</p>
 {/if}

7514a0e39ffbf34935b6216a5e8b7802d7dd5dac
엘풍 <eruhoon@gmail.com>
2021-09-14 21:50:36 +0900

    update EmbedRoute

--- src/app/view/route/EmbedRoute.svelte
+++ src/app/view/route/EmbedRoute.svelte
@@ -1,1 +1,11 @@
-<div>힝</div>
+<script>
+  const urlParams = new URLSearchParams(window.location.search);
+  const hasId = urlParams.has('id');
+  const id = urlParams.get('id');
+</script>
+
+{#if hasId}
+  <p>{id}</p>
+{:else}
+  <p>error</p>
+{/if}

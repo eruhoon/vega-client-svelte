ef5b0c740d61a658903624965724ee7686a0d592
엘풍 <eruhoon@gmail.com>
2021-11-17 20:45:50 +0900

    update rollup setting

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import json from "@rollup/plugin-json";
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
@@ -64,6 +65,7 @@ export default {
       dedupe: ['svelte'],
     }),
     commonjs(),
+    json(),
     typescript({
       sourceMap: !production,
       inlineSources: !production,

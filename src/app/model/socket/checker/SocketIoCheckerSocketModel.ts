67a175c2cf23e2a1232f11005acfe3af05302482
엘풍 <eruhoon@gmail.com>
2021-12-12 22:58:10 +0900

    add Push for NewStream

--- src/app/model/socket/checker/SocketIoCheckerSocketModel.ts
+++ src/app/model/socket/checker/SocketIoCheckerSocketModel.ts
@@ -1,8 +1,12 @@
 import io from 'socket.io-client';
+import { HashGenerator } from '../../../util/hash/HashGenerator';
+import { PushListService } from '../../../view/main/push/PushListService';
 import type {
   CheckerSocketModel,
   OnRefreshStreams,
 } from './CheckerSocketModel';
+
+const KEY_NEW_STREAM = 'new_stream_notification';
 
 export class SocketIoCheckerSocketModel implements CheckerSocketModel {
   #socket: SocketIOClient.Socket;
@@ -16,8 +20,17 @@ export class SocketIoCheckerSocketModel implements CheckerSocketModel {
     this.#socket.on('refresh_streams', (raw: any) => {
       this.#onRefreshStream(raw);
     });
-    this.#socket.on('new_stream_notification', (raw: any) => {
-      console.log('new_stream_notification', raw);
+    this.#socket.on(KEY_NEW_STREAM, (raw: NewStreamNotificationParam) => {
+      PushListService.push({
+        hash: new HashGenerator().generate('newStreamNotification'),
+        title: '새 방송 알림',
+        icon: raw.icon,
+        body: `${raw.nickname} 방송을 시작합니다.`,
+        mute: true,
+        channel: 'local-stream',
+        read: false,
+        timestamp: new Date().getTime(),
+      });
     });
   }
 
@@ -32,3 +45,17 @@ export class SocketIoCheckerSocketModel implements CheckerSocketModel {
     return io.connect(`https://${host}?key=${privateKey}`);
   }
 }
+
+type NewStreamNotificationParam = {
+  description: string;
+  icon: string;
+  keyid: string;
+  nickname: string;
+  onair: boolean;
+  platform: string;
+  result: boolean;
+  thumbnail: string;
+  title: string;
+  url: string;
+  viewer: number;
+};

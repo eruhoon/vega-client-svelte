e737349f7d1183ea57d24e82e63a4dfb2f092bb7
엘풍 <eruhoon@gmail.com>
2021-12-22 22:56:50 +0900

    update layout for MemoPack

--- src/app/view/chat/pack/MemoPack.svelte
+++ src/app/view/chat/pack/MemoPack.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import GeneralPurposeCard from './gpc/GeneralPurposeCard.svelte';
+
   export let body: string;
   let icon: string;
   let nickname: string;
@@ -11,22 +13,9 @@
   }
 </script>
 
-<div class="root">
-  <img class="icon" src={icon} alt="{nickname} 아이콘" />
-  <div class="content">
-    <div>{nickname}</div>
-    <div>{content}</div>
-  </div>
-</div>
-
-<style lang="scss">
-  .root {
-    width: 100%;
-    display: flex;
-    align-items: center;
-    .icon {
-      width: 50px;
-      height: 50px;
-    }
-  }
-</style>
+<GeneralPurposeCard
+  {icon}
+  title={nickname}
+  subtitle={content}
+  orientation="horizontal"
+/>

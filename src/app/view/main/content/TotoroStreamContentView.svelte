a841518711905ff77fd5dca0135a252cdc266ced
엘풍 <eruhoon@gmail.com>
2021-12-26 19:03:48 +0900

    fix bug

--- src/app/view/main/content/TotoroStreamContentView.svelte
+++ src/app/view/main/content/TotoroStreamContentView.svelte
@@ -3,7 +3,7 @@
 
   import FlvContentView from './FlvContentView.svelte';
 
-  export let src: LocalStreamSource = { icon: '', url: '' };
+  export let src: LocalStreamSource = { icon: '', url: '', title: '' };
 
   $: url = `https://parasite.banjai.tv/live/${src.url}.flv`;
   $: icon = src?.icon;

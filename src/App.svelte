dbc4b586e6e8352ddb57573e3340f548a996e0f5
엘풍 <eruhoon@gmail.com>
2021-12-31 14:58:49 +0900

    update Route

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Route,Router } from 'svelte-routing';
+  import { Route, Router } from 'svelte-routing';
   import ChatRoute from './app/view/route/ChatRoute.svelte';
   import EmbedRoute from './app/view/route/EmbedRoute.svelte';
   import MainRoute from './app/view/route/MainRoute.svelte';
@@ -8,6 +8,6 @@
 
 <Router {url}>
   <Route path="/"><MainRoute /></Route>
-  <Route paht="/embed"><EmbedRoute /></Route>
-  <Route paht="/chat"><ChatRoute /></Route>
+  <Route path="/embed"><EmbedRoute /></Route>
+  <Route path="/chat"><ChatRoute /></Route>
 </Router>

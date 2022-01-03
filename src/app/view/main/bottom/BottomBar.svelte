1e6a7d8f07f58fb8d61fe128111a7083d9c3369b
엘풍 <eruhoon@gmail.com>
2022-01-03 23:23:24 +0900

    update BottomBar and StreamList

--- src/app/view/main/bottom/BottomBar.svelte
+++ src/app/view/main/bottom/BottomBar.svelte
@@ -1,5 +1,39 @@
-<script>
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { OptionService } from '../../../service/OptionService';
+
   import StreamList from '../../stream/StreamList.svelte';
+  import ChristmasEventItem from './ChristmasEventItem.svelte';
+  import HappyNewYearEventItem from './HappyNewYearEventItem.svelte';
+
+  let rightAlign: boolean = false;
+
+  onMount(() => {
+    OptionService.enableCheckerRightAlign.subscribe((it) => {
+      rightAlign = it;
+    });
+  });
 </script>
 
-<StreamList />
+<div class="bottom-bar" class:right-align={rightAlign}>
+  <StreamList />
+  <ChristmasEventItem />
+  <HappyNewYearEventItem />
+</div>
+
+<style lang="scss">
+  .bottom-bar {
+    display: flex;
+    justify-content: flex-start;
+    height: 100%;
+    width: 100%;
+    background: var(--primary-background-color);
+    border: 1px solid var(--primary-hoverground-color);
+
+    box-shadow: var(--primary-box-shadow);
+
+    &.right-align {
+      justify-content: flex-end;
+    }
+  }
+</style>
--- src/app/view/stream/StreamList.svelte
+++ src/app/view/stream/StreamList.svelte
@@ -1,71 +1,34 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
   import type { StreamInfo } from '../../model/stream/StreamInfo';
   import { FavoriteService } from '../../service/FavoriteService';
-  import { OptionService } from '../../service/OptionService';
   import { StreamService } from '../../service/StreamService';
-  import ChristmasEventItem from '../main/bottom/ChristmasEventItem.svelte';
-  import HappyNewYearEventItem from '../main/bottom/HappyNewYearEventItem.svelte';
   import StreamEntry from './StreamEntry.svelte';
 
   let entries: StreamInfo[] = [];
   let externals: StreamInfo[] = [];
   let favorites: StreamInfo[];
-  let rightAlign: boolean = false;
 
   $: favorites = externals.filter((it) => {
     const { platform, keyid: keyId } = it;
     return FavoriteService.isFavorite(platform, keyId);
   });
 
-  StreamService.locals.subscribe((v) => (entries = v));
-  StreamService.externals.subscribe((it) => (externals = it));
-  OptionService.enableCheckerRightAlign.subscribe((it) => {
-    rightAlign = it;
-    console.log(it);
+  $: locals = entries.filter((it) => it.platform === 'local');
+  $: notLocals = entries.filter((it) => it.platform !== 'local');
+
+  onMount(() => {
+    StreamService.locals.subscribe((v) => (entries = v));
+    StreamService.externals.subscribe((it) => (externals = it));
   });
 </script>
 
-<div class="stream-list" class:right-align={rightAlign}>
-  {#each entries as b}
-    <div class="stream-item">
-      <StreamEntry stream={b} />
-    </div>
-  {/each}
-  {#each favorites as favorite}
-    <div class="stream-item">
-      <StreamEntry stream={favorite} />
-    </div>
-  {/each}
-  <ChristmasEventItem />
-  <HappyNewYearEventItem />
-</div>
-
-<style lang="scss">
-  .stream-list {
-    display: flex;
-    justify-content: flex-start;
-    height: 100%;
-    width: 100%;
-    background: var(--primary-background-color);
-    border: 1px solid var(--primary-hoverground-color);
-
-    box-shadow: var(--primary-box-shadow);
-
-    &.right-align {
-      justify-content: flex-end;
-    }
-  }
-
-  .stream-item {
-    display: inline-block;
-    margin: 0;
-
-    &:first-child {
-      margin-inline-start: 2.5px;
-    }
-
-    &:last-child {
-      margin-inline-end: 2.5px;
-    }
-  }
-</style>
+{#each locals as stream}
+  <StreamEntry {stream} />
+{/each}
+{#each notLocals as stream}
+  <StreamEntry {stream} />
+{/each}
+{#each favorites as favorite}
+  <StreamEntry stream={favorite} />
+{/each}

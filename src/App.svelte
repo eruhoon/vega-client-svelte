2d012ef3f2eff12b5ddb54b6fc4c32582caeade8
엘풍 <eruhoon@gmail.com>
2022-01-16 22:01:53 +0900

    add ObsChatRoute

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
   import ChatRoute from './app/view/route/ChatRoute.svelte';
   import EmbedRoute from './app/view/route/EmbedRoute.svelte';
   import MainRoute from './app/view/route/MainRoute.svelte';
+  import ObsChatRoute from './app/view/route/ObsChatRoute.svelte';
   export let url = '';
 </script>
 
@@ -10,4 +11,5 @@
   <Route path="/"><MainRoute /></Route>
   <Route path="/embed"><EmbedRoute /></Route>
   <Route path="/chat"><ChatRoute /></Route>
+  <Route path="/obs/chat"><ObsChatRoute /></Route>
 </Router>
---
+++ src/app/view/route/ObsChatRoute.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  const urlParams = new URLSearchParams(window.location.search);
+  const hasSecret = urlParams.has('secret');
+  const secret = urlParams.get('secret');
+</script>
+
+{#if hasSecret}
+  <p>{secret}</p>
+{:else}
+  <p>Error: Invalid Secret Key</p>
+{/if}

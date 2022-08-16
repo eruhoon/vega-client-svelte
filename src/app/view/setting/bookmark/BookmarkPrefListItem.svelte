398101358a1962c8ab0f5c351157a207ac0a3598
w2story(이건희) <w2story@naver.com>
2022-08-16 23:09:45 +0900

    22-08-16-bookmark-modal-style-updae

--- src/app/view/setting/bookmark/BookmarkPrefListItem.svelte
+++ src/app/view/setting/bookmark/BookmarkPrefListItem.svelte
@@ -1,55 +1,94 @@
 <script lang="ts">
-    import type { Bookmark } from '../../../model/bookmark/Bookmark';
-    import { BookmarkService } from '../../../service/BookmarkService';
+  import type { Bookmark } from '../../../model/bookmark/Bookmark';
+  import { BookmarkService } from '../../../service/BookmarkService';
 
-    export let bookmark: Bookmark;
+  export let bookmark: Bookmark;
 
-    function onDeleteClick() {
-        BookmarkService.removeBookmark(bookmark);
-    }
+  function onDeleteClick() {
+    BookmarkService.removeBookmark(bookmark);
+  }
 </script>
 
 <div class="item">
-    <div class="title">
-        <!-- svelte-ignore a11y-missing-attribute -->
-        <img class="icon" src={bookmark.icon} />
-        <p>{bookmark.title}</p>
-    </div>
+  <div class="title">
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <img class="icon" src={bookmark.icon} />
+    <p>{bookmark.title}</p>
+  </div>
 
-    <div class="button" on:click={(_) => onDeleteClick()}>
-        <button>delete</button>
-    </div>
+  <button class="btn" on:click={(_) => onDeleteClick()}>
+    <i class="fas fa-times svelte-gc06z3" aria-hidden="true" />
+  </button>
 </div>
 
 <style lang="scss">
-    .item {
-        width: 100%;
-        height: 42px;
-        padding-bottom: 10px;
-        padding-top: 10px;
-        display: flex;
+  .item {
+    width: calc(100% + 30px);
+    height: 50px;
+    padding-bottom: 10px;
+    padding-top: 10px;
 
-        &:first-child {
-            padding-top: 0px;
-        }
+    margin-left: -15px;
+    border-bottom: 1px solid var(--primary-hoverground-color);
 
-        .title {
-            width: 80%;
-            height: 42px;
-            float: left;
+    display: flex;
 
-            .icon {
-                float: left;
-                width: 42px;
-                height: 42px;
-            }
-
-            p {
-                margin-left: 10px;
-                font-size: 18px;
-                padding: 12px;
-                color: var(--primary-foreground-color);
-            }
-        }
+    &:first-child {
+      padding-top: 0px;
     }
+
+    .title {
+      width: 80%;
+      height: 50px;
+      float: left;
+
+      .icon {
+        float: left;
+        width: 50px;
+        height: 50px;
+        margin-right: 15px;
+      }
+
+      p {
+        font-size: 18px;
+        padding: 12px;
+        color: var(--primary-foreground-color);
+      }
+    }
+    .btn {
+      width: 50px;
+      height: 50px;
+      margin-right: 15px;
+
+      background-color: var(--primary-hoverground-color);
+      border: 1px solid var(--primary-hoverground-color);
+      border-radius: 0%;
+      text-align: center;
+
+      i {
+        font-size: 24px;
+        line-height: 40px;
+        padding-left: 1px;
+        color: var(--primary-foreground-color);
+      }
+
+      &:hover {
+        background-color: var(--primary-activeground-color);
+        border: 1px solid var(--primary-activeground-color);
+
+        i {
+          color: var(--primary-activeground-font-color);
+        }
+      }
+
+      &:active {
+        background-color: var(--primary-activeground-color);
+        border-color: var(--primary-activeground-color);
+
+        i {
+          color: var(--primary-activeground-font-color);
+        }
+      }
+    }
+  }
 </style>
--- src/app/view/setting/bookmark/BookmarkTextInput.svelte
+++ src/app/view/setting/bookmark/BookmarkTextInput.svelte
@@ -1,37 +1,37 @@
 <script lang="ts">
-    export let title = '';
-    export let subtitle = '';
-    export let value = '';
-    export let name = '';
-    export let placeholder = '';
+  export let title = '';
+  export let subtitle = '';
+  export let value = '';
+  export let name = '';
+  export let placeholder = '';
 </script>
 
 <div class="input-group">
-    <p>
-        {title}
-        {#if subtitle}<small>({subtitle})</small>{/if}
-    </p>
-    <input type="text" bind:value {name} {placeholder} />
+  <p>
+    {title}
+    {#if subtitle}<small>({subtitle})</small>{/if}
+  </p>
+  <input type="text" bind:value {name} {placeholder} />
 </div>
 
 <style lang="scss">
-    .input-group {
-        width: 100%;
+  .input-group {
+    width: 100%;
+    margin-bottom: 15px;
 
-        p {
-            font-size: 10px;
-            line-height: 18px;
-            margin-bottom: 5px;
-        }
-
-        input {
-            width: 100%;
-            background: var(--primary-hoverground-color);
-            border: 0px;
-            border-radius: 0%;
-            color: var(--primary-foreground-color);
-            padding: 5px 5px;
-            font-size: 10px;
-        }
+    p {
+      font-size: 14px;
+      line-height: 16px;
+      margin-bottom: 5px;
     }
+
+    input {
+      width: 100%;
+      background: var(--primary-hoverground-color);
+      border: 0px;
+      border-radius: 0%;
+      color: var(--primary-foreground-color);
+      padding: 10px 10px;
+    }
+  }
 </style>

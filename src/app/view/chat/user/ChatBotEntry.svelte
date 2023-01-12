1ba1f261dcdef00871c3d4afdb867511282c0700
엘풍 <eruhoon@gmail.com>
2023-01-12 22:44:38 +0900

    update layout of ChatBotEntry

---
+++ src/app/view/chat/user/ChatBotEntry.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import ChatSenderEntry from './ChatSenderEntry.svelte';
+
+  export let hash: string;
+  export let icon: string;
+  export let nickname: string;
+</script>
+
+<ChatSenderEntry {hash} {icon} {nickname}>
+  <i class="fas fa-bell active" />
+  <i class="fas fa-question active" />
+</ChatSenderEntry>
+
+<style lang="scss">
+  i {
+    width: 20px;
+    text-align: center;
+    font-size: 16px;
+    padding: 7px 0px 7px 5px;
+    float: right;
+    color: var(--primary-hoverground-color);
+  }
+
+  i.active {
+    color: var(--primary-foreground-color);
+  }
+</style>
--- src/app/view/chat/user/ChatBotList.svelte
+++ src/app/view/chat/user/ChatBotList.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { ChatNetworkService } from '../../../service/ChatNetworkService';
-  import ChatSenderEntry from './ChatSenderEntry.svelte';
+  import ChatBotEntry from './ChatBotEntry.svelte';
   import ChatSenderList from './ChatSenderList.svelte';
 
   let bots: {
@@ -14,6 +14,6 @@
 
 <ChatSenderList title="봇 리스트" length={bots.length}>
   {#each bots as { hash, icon, nickname }}
-    <ChatSenderEntry {hash} {icon} {nickname} />
+    <ChatBotEntry {hash} {icon} {nickname} />
   {/each}
 </ChatSenderList>

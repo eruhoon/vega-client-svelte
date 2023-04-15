def39d47409ec9be25d039e7beccc400c914e8ff
엘풍 <eruhoon@gmail.com>
2023-04-15 14:15:05 +0900

    add ObsDonationRoute

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,7 @@
   import EmbedRoute from './app/view/route/EmbedRoute.svelte';
   import MainRoute from './app/view/route/MainRoute.svelte';
   import ObsChatRoute from './app/view/route/ObsChatRoute.svelte';
+  import ObsDonationRoute from './app/view/route/ObsDonationRoute.svelte';
   export let url = '';
 
   let keyCount = 0;
@@ -27,4 +28,5 @@
   <Route path="/embed"><EmbedRoute /></Route>
   <Route path="/chat"><ChatRoute /></Route>
   <Route path="/obs/chat"><ObsChatRoute /></Route>
+  <Route path="/obs/donation"><ObsDonationRoute /></Route>
 </Router>
